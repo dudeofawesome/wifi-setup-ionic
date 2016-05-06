1da3594d229712f85cc5b62d8dc386bc0327b866
Louis Orleans <louis@orleans.io>
2016-05-05 18:34:55 -0700

    Trying to use ionic-native/hotspot

--- app/components/wifi-setup/wifi-setup.ts
+++ app/components/wifi-setup/wifi-setup.ts
@@ -1,26 +1,45 @@
-import {Component, Inject} from 'angular2/core';
-import {Hotspot, Network, Connection} from 'ionic-native';
+import {Component, Inject, Input} from 'angular2/core';
+import {Hotspot} from 'ionic-native';
 
-/*
-Generated class for the WifiSetup component.
-
-See https://angular.io/docs/ts/latest/api/core/ComponentMetadata-class.html
-for more info on Angular 2 Components.
-*/
 @Component({
     selector: 'wifi-setup',
     templateUrl: 'build/components/wifi-setup/wifi-setup.html',
     // styleUrls: ['build/components/wifi-setup/wifi-setup.css']
 })
 export class WifiSetup {
-    text: string = '';
+    @Input() IP: string;
+    @Input('service-name') serviceName: string = '';
 
-    constructor(@Inject(Hotspot) hotspot: Hotspot) {
-        this.text = 'Hello World';
+    err: string = '';
 
-        debugger;
-        Hotspot.scanWifi().then((a, b, c) => {
+    private networks: Array<{
+        SSID: string,
+        BSSID: string,
+        frequency: number,
+        level: number,
+        timestamp: number,
+        capabilities: string
+    }>;
+
+    constructor() {
+        Hotspot.scanWifi().then((networks) => {
+            this.networks = networks;
             debugger;
+        }).catch((err) => {
+            if (err === 'cordova_not_available') {
+                this.networks = [
+                    {
+                        SSID: 'string',
+                        BSSID: 'string',
+                        frequency: 0,
+                        level: 0,
+                        timestamp: 0,
+                        capabilities: 'string'
+                    }
+                ]
+            } else {
+                this.serviceName = err;
+            }
         });
     }
 }
